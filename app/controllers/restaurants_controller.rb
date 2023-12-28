968a1898b8eddc43d0b4be5af3fb33553976e41c
andres ibanez <andresibanezulloa@gmail.com>
2023-12-27 23:21:32 -0300

    add active storage, cloudinary

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    # @restaurants = [0x0334, 0x34635]
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user_id = current_user.id
+    if @restaurant.save 
+      redirect_to restaurants_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private 
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :restaurants
+  has_one_attached :photo # Es una columna virtual creada en users
 end
